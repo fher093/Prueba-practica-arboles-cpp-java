c59d5673658e07a7d44540c5a2a5566b874f58bd
Fernando <fherdavidlleru@gmail.com>
2026-05-15 07:32:09 -0500

    actualizacion estudinate

---
+++ java/Estudiante.java
@@ -0,0 +1,11 @@
+public class Estudiante {
+    String cedula;
+    String nombre;
+    double nota;
+
+    public Estudiante(String cedula, String nombre, double nota) {
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.nota = nota;
+    }
+}
