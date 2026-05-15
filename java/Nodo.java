d04d9577c2670ef57c51baa99c9472a2af5a204d
Fernando <fherdavidlleru@gmail.com>
2026-05-15 07:33:28 -0500

    clase nodo

---
+++ java/Nodo.java
@@ -0,0 +1,11 @@
+public class Nodo {
+    Estudiante dato;
+    Nodo izquierda;
+    Nodo derecha;
+
+    public Nodo(Estudiante dato) {
+        this.dato = dato;
+        izquierda = null;
+        derecha = null;
+    }
+}
