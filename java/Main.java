388853ef2fae82924b36d5e6e9bef2f3e3254c58
Fernando <fherdavidlleru@gmail.com>
2026-05-15 07:42:09 -0500

    arreglo en la clase main

--- java/Main.java
+++ java/Main.java
@@ -1,24 +1,77 @@
 import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        ArbolBST arbol = new ArbolBST();
+
+        int opcion;
+
+        do {
+
+            System.out.println("\n===== MENU =====");
+            System.out.println("1. Insertar");
+            System.out.println("2. Buscar");
+            System.out.println("3. Eliminar");
+            System.out.println("4. Inorden");
+            System.out.println("5. Preorden");
+            System.out.println("6. Postorden");
+            System.out.println("7. Por niveles");
+            System.out.println("8. Contar nodos");
+            System.out.println("9. Altura");
+            System.out.println("10. Nota mayor");
+            System.out.println("11. Nota menor");
+            System.out.println("12. Aprobados");
+            System.out.println("13. Reprobados");
+            System.out.println("14. Salir");
+
+            System.out.print("Opcion: ");
+            opcion = sc.nextInt();
+
+            sc.nextLine();
+
+            switch (opcion) {
+
+                case 1:
+
+                    System.out.print("Cedula: ");
+                    String cedula = sc.nextLine();
+
+                    System.out.print("Nombre: ");
+                    String nombre = sc.nextLine();
+
                     double nota;
 
                     do {
+
                         System.out.print("Nota (0-10): ");
                         nota = sc.nextDouble();
+
                     } while (nota < 0 || nota > 10);
 
                     sc.nextLine();
 
-                    arbol.insertarEstudiante(new Estudiante(cedula, nombre, nota));
+                    arbol.insertarEstudiante(
+                            new Estudiante(cedula, nombre, nota)
+                    );
+
                     break;
 
                 case 2:
+
                     System.out.print("Cedula: ");
                     arbol.buscarEstudiante(sc.nextLine());
+
                     break;
 
                 case 3:
+
                     System.out.print("Cedula a eliminar: ");
                     arbol.eliminarEstudiante(sc.nextLine());
+
                     break;
 
                 case 4:
